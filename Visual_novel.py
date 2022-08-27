fd9054ec44668ca37e9957e88c163f494ba6bc0e
MasterCrafter69 <111368986+MasterCrafter69@users.noreply.github.com>
2022-08-27 00:00:13 -0500

    Add files via upload

--- Visual_novel.py
+++ Visual_novel.py
@@ -1,6 +1,6 @@
 
-Monedas = 100
-Buenas_acciones = 100
+monedas = 100
+buenas_acciones = 100
 
 print("Había una vez un caballero medieval genérico, que como todo caballero medieval genérico debía rescatar\n"
       "a la princesa que estaba encerrada en la cima de una torre custodiada por varios dragones.\n")
@@ -13,13 +13,14 @@ print("Así que el caballero salió a su aventura...\n")
 print("Cuando el caballero entro al bosque se encontró con un vendedor que le ofrecia una espada mas grande\n"
       "por el precio de 50 monedas...\n")
 
-Pregunta_espada = input("¿Aceptas la oferta?\n")
-if Pregunta_espada == ("si"):
-      Monedas = Monedas - 50
+pregunta_espada = input("¿Aceptas la oferta? Si/No\n")
+if pregunta_espada == ("si"):
+      monedas = monedas - 50
       print("Cantidad de monedas:", Monedas)
-      print("Y una espada nueva")
+      print("Y una espada nueva\n")
 
-elif Pregunta_espada == ("no"):
-      print("se va")
-
+elif pregunta_espada == ("no"):
+      monedas = monedas - 25
+      print("El vendedor alcanza a quitarte unas monedas y se va corriendo")
+      print("Cantidad de monedas:", monedas)
 
